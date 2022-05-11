491f9bf968a991743f6d5e2ca4f74818589f96e6
super-f-3000 <81982904+super-f-3000@users.noreply.github.com>
2022-05-11 07:48:59 -0400

    Update espjs.js

--- build/espjs.js
+++ build/espjs.js
@@ -4,6 +4,7 @@ const nulo = null;
 const indefinido = undefined;
 const NuN = NaN;
 const consola = {
+    "advertir": function (message) {console.warn(message)},
     "imprimir": function (message) {console.log(message)},
     "vaciar": function () {console.clear()}
 }
